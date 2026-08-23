c35699169a391bc1e4c17d6b687f200beb6fb60f
inokbh <inok.hapuhinna@gmail.com>
2026-08-23 13:26:57 +0530

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -18,12 +18,13 @@ import {
 } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyDfKwZVp7_berT1hhOcVnlkgsN1ceifyD4",
+  authDomain: "praleesha-ab618.firebaseapp.com",
+  databaseURL: "https://praleesha-ab618-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "praleesha-ab618",
+  storageBucket: "praleesha-ab618.firebasestorage.app",
+  messagingSenderId: "72038159089",
+  appId: "1:72038159089:web:06cb6fb930feeb05f18f50"
 };
 
 export const firebaseApp = initializeApp(firebaseConfig);
